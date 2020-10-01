74503087c5adc6c8a664fd48200f852bacc0488a
Bussler <maarten.bussler@gmail.com>
2020-10-01 21:36:20 +0200

    Class oriented approach for UI and PdfLoading handling

--- PdfViewer/PdfViewer/MainPage.xaml.cs
+++ PdfViewer/PdfViewer/MainPage.xaml.cs
@@ -50,13 +50,10 @@ namespace PdfViewer
 
             if (file == null) return;
 
-            await OpenPDFAsync(file); //open pdf
-            await LoadPages(); //load into xml
+            PdfLoader loader = new PdfLoader(file, PdfPages); //handles loading of pfd file into the xml list PdfPages
 
         }
 
-        private PdfDocument myPdfDoc { get; set; } //loaded pdf
-        private uint currentPage = 0; //index for single pages loading
 
         public ObservableCollection<BitmapImage> PdfPages //collection to hold currenrly displayed pdf images
         {
@@ -64,74 +61,5 @@ namespace PdfViewer
             set;
         } = new ObservableCollection<BitmapImage>();
 
-        private async Task OpenPDFAsync(StorageFile file) //opens specified pdf and stores it in local var for later use
-        {
-            if (file == null) throw new ArgumentNullException();
-
-            myPdfDoc = await PdfDocument.LoadFromFileAsync(file);
-        }
-
-        private async Task LoadPages() //loads all pages in the current pdf into xml scroll view
-        {
-            if (myPdfDoc == null) //TODO better error checking - now we just crash the program
-            {
-                throw new Exception("No document open.");
-            }
-
-            PdfPages.Clear();//yeetus deletus
-
-            for(uint i = 0; i < myPdfDoc.PageCount; i++) //iterate over all pages and blend them into the scroll view
-            {
-                BitmapImage image = new BitmapImage(); //image to blend current pdf page into
-                var curPage = myPdfDoc.GetPage(i); 
-
-                using (var stream = new InMemoryRandomAccessStream())//blend content of pdf page into the image
-                {
-                    await curPage.RenderToStreamAsync(stream);
-                    await image.SetSourceAsync(stream);
-                }
-                PdfPages.Add(image); //add new image to collection in order to display it in the xml
-
-            }
-            Debug.WriteLine("PDF loading done, yip yip!");
-        }
-
-        //stuff for single page loading OUTDATED
-        private async Task DisplayPage(uint pageIndex) //render a single page specified by param
-        {
-            if (myPdfDoc == null)
-            {
-                throw new Exception("No document open.");
-            }
-
-            if (pageIndex >= myPdfDoc.PageCount || pageIndex < 0)
-            {
-                throw new ArgumentOutOfRangeException($"Document has only {myPdfDoc.PageCount} pages.");
-            }
-
-            var page = myPdfDoc.GetPage(pageIndex); //grab page to render
-
-            var image = new BitmapImage(); //image to render into
-
-            using (var stream = new InMemoryRandomAccessStream()) //blend pdf page into image
-            {
-                await page.RenderToStreamAsync(stream);
-                await image.SetSourceAsync(stream);
-
-                PdfImage.Source = image; //set cml-image source to the new image
-            }
-        }
-
-        private async void Next_Click(object sender, RoutedEventArgs e)
-        {
-            this.currentPage++;
-            await DisplayPage(this.currentPage);
-        }
-
-        private async void Prev_Click(object sender, RoutedEventArgs e)
-        {
-            this.currentPage--;
-            await DisplayPage(this.currentPage);
-        }
     }
 }
---
+++ PdfViewer/PdfViewer/PdfLoader.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Pdf;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace PdfViewer
+{
+    class PdfLoader
+    {
+        //attributes
+        private PdfDocument myPdfDoc { get; set; } //loaded pdf
+
+        public ObservableCollection<BitmapImage> PdfPages //collection to hold currenrly displayed pdf images
+        {
+            get;
+            set;
+        } = new ObservableCollection<BitmapImage>();
+
+        //constructor
+        public PdfLoader(StorageFile file, ObservableCollection<BitmapImage> PdfPages)
+        {
+            if (file == null)
+                return;
+            this.PdfPages = PdfPages;
+            loadPdf(file);
+        }
+
+        private async void loadPdf(StorageFile file)
+        {
+            await OpenPDFAsync(file); //open pdf
+            await LoadPages(); //load into xml
+        }
+
+        //methods
+        private async Task OpenPDFAsync(StorageFile file) //opens specified pdf and stores it in local var for later use
+        {
+            if (file == null) throw new ArgumentNullException();
+
+            myPdfDoc = await PdfDocument.LoadFromFileAsync(file);
+        }
+
+        private async Task LoadPages() //loads all pages in the current pdf into xml scroll view
+        {
+            if (myPdfDoc == null) //TODO better error checking - now we just crash the program
+            {
+                throw new Exception("No document open.");
+            }
+
+            PdfPages.Clear();//yeetus deletus
+
+            for (uint i = 0; i < myPdfDoc.PageCount; i++) //iterate over all pages and blend them into the scroll view
+            {
+                BitmapImage image = new BitmapImage(); //image to blend current pdf page into
+                var curPage = myPdfDoc.GetPage(i);
+
+                using (var stream = new InMemoryRandomAccessStream())//blend content of pdf page into the image
+                {
+                    await curPage.RenderToStreamAsync(stream);
+                    await image.SetSourceAsync(stream);
+                }
+                PdfPages.Add(image); //add new image to collection in order to display it in the xml
+               // mp.PdfPages.Add(image);
+            }
+            Debug.WriteLine("PDF loading done, yip yip!");
+        }
+
+    }
+}
